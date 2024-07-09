3f6bb5daf3b68af55e786ac1042602e2efdfa228
Ankush Verma <ankushvarma006@gmail.com>
2024-07-09 22:15:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const Product = require('./models/product.model');
+const app = express()
+
+//Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+//routes
+
+app.get('/', (req, res) => {
+    res.send("Hello from NOde API updated");
+})
+
+app.post('/api/create/products', async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+app.get('/api/get/products', async (req, res) => {
+    try {
+        const product = await Product.find({});
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+app.get('/api/get/product/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+// UPDATE PRODUCT API
+app.put('/api/update/product/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+
+        if (!product) {
+            return res.status(404).json({ message: "Product Not Found" })
+        }
+
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+// DELETE PRODUCT API
+
+app.delete('/api/delete/product/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndDelete(id);
+
+        if (!product) {
+            return res.status(404).json({ message: "Product Not Found" })
+        }
+
+
+        res.status(200).json({ message: "Product deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+})
+
+mongoose.connect('mongodb+srv://ankushvarma006:R879ZdH1br9Xt551@cluster0.s3i3kec.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0')
+    .then(() => {
+        console.log('Connected to database!');
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
+        });
+    })
+    .catch(() => {
+        console.log("Connection failed!")
+    });
